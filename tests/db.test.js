b40121a1cf5d7363d3da9ea2708cd6b91c88ad4e
ayanakkabi1 <aynour200609@gmail.com>
2026-09-15 14:38:04 +0100

    HOS-10 HOS-13 feat: initialize project with package.json and add database tests
    - Created package.json with dependencies and scripts for the project.
    - Added automated tests for database connection and queries in db.test.js.

---
+++ tests/db.test.js
@@ -0,0 +1,23 @@
+import pool from "../config/db.js";
+
+describe('Tests Automatisés de la Base de Données',()=>{
+    afterAll(async () => {
+        await pool.end();
+    });
+    test('Doit se connecter avec succès à la base de données MySQL', async () => {
+        const connection = await pool.getConnection();
+        expect(connection).toBeDefined();
+        connection.release();
+    });
+
+    test('Doit exécuter une requête SQL simple (SELECT 1 + 1)', async () => {
+        const [rows] = await pool.query('SELECT 1 + 1 AS result');
+        expect(rows[0].result).toBe(2);
+    });
+
+    test('Doit vérifier la présence de la table users', async () => {
+        const [tables] = await pool.query("SHOW TABLES LIKE 'users'");
+        expect(tables.length).toBeGreaterThan(0);
+    });
+})
+
